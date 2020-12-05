8e3ec03ea47e7d8a615bc2c598f4c543163ae869
Andrii Girnyi <andriy.police91@gmail.com>
2020-12-05 13:33:51 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+    
+
+
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        asNavFor: '.heroes__slider-text',
+        prevArrow:'<button class="slick-btn slick-prev"><img src="../viking/images/right.png" alt="prew"></button>',
+        nextArrow:'<button class="slick-btn slick-next"><img src="../viking/images/left.png" alt="next"></button>',
+      });
+      $('.heroes__slider-text').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          asNavFor: '.heroes__slider-img',
+        fade: true,
+        arrows: false
+      });
+});
+
+
